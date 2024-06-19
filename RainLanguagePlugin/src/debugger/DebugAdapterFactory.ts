7af12915d8b983c7a9717ef59fa06cdf1a56d98a
渴望蓝天 <1457675796@qq.com>
2024-06-20 07:20:02 +0800

    将插件和LS独立出去

--- RainLanguagePlugin/src/debugger/DebugAdapterFactory.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-import { RainDebugSession } from './DebugSession';
-import { RainDebugConfiguration } from './DebugConfigurationProvider';
-
-export class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory{
-    createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-        return new vscode.DebugAdapterInlineImplementation(new RainDebugSession(session.configuration as RainDebugConfiguration));
-    }
-}
--- RainLanguageServer/HoverInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RainLanguageServer
-{
-    internal readonly struct HoverInfo(TextRange range, string info, bool markdown)
-    {
-        public readonly TextRange range = range;
-        public readonly string info = info;
-        public readonly bool markdown = markdown;
-    }
-}
